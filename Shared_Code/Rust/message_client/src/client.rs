9801dce3ae933f37facdf507771e5fe1befd27db
exlted <benk@exlted.com>
2024-07-05 11:05:38 -0500

    Initial Commit Supporting Multiple Channels

--- Shared_Code/Rust/message_client/src/client.rs
+++ Shared_Code/Rust/message_client/src/client.rs
@@ -1,44 +1,164 @@
 use amqprs::{
     channel::{
-        BasicPublishArguments, BasicConsumeArguments, QueueBindArguments, QueueDeclareArguments
+        Channel as AMQPRS_Channel,
+        BasicPublishArguments, BasicConsumeArguments, QueueBindArguments, QueueDeclareArguments, ExchangeDeclareArguments
     },
-    connection::OpenConnectionArguments
-};
-use std::mem;
-use std::ptr;
-use async_trait::async_trait;
-use std::sync::Arc;
-use tokio::sync::Mutex;
-use amqprs::{
-    callbacks::{ChannelCallback, ConnectionCallback},
-    channel::Channel,
-    connection::Connection,
+    connection::{
+        Connection,
+        OpenConnectionArguments
+    },
     error::Error,
+    callbacks::{ChannelCallback, ConnectionCallback},
     Close, Ack, Nack, Return, Cancel, CloseChannel, BasicProperties,
     consumer::AsyncConsumer
 };
-
-use amqprs::channel::ExchangeDeclareArguments;
-use tokio::time;
+use std::{
+    mem,
+    ptr,
+    sync::Arc
+};
+use tokio::{
+    sync::Mutex,
+    time
+};
+use async_trait::async_trait;
 
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Channel {
+    connection: Arc<Mutex<Client>>,
+    queue_name: String,
+    pub(crate) channel: Option<AMQPRS_Channel>,
+    pub(crate) open: bool,
+    pub(crate) flow_blocked: bool,
+    pub exchange_name: String,
+    pub routing_key: String,
+}
+
+impl Channel {
+
+    pub(crate) async fn new(connection: Arc<Mutex<Client>>, exchange_name: String, routing_key: String) -> Arc<Mutex<Channel>>
+    {
+        let channel = Arc::new(Mutex::new(Channel {
+            connection,
+            queue_name: "".to_string(),
+            channel: None,
+            open: false,
+            flow_blocked: false,
+            exchange_name,
+            routing_key,
+        }));
+
+        CHANNELS.lock().await.push(channel.clone());
+
+        return channel
+    }
+
+    pub(crate) async fn open_channel<F>(&mut self, channel_callback: F) -> Option<()>
+        where F: ChannelCallback + Send + 'static,
+    {
+         self.channel = Some(self.connection.lock().await.connection.as_ref()?.open_channel(None).await.unwrap());
+         self.channel.as_ref()?
+             .register_callback(channel_callback)
+             .await
+             .unwrap();
+
+         let mut exchange_opts = ExchangeDeclareArguments::new(&self.exchange_name.clone(), "topic");
+         exchange_opts.durable = true;
+
+         self.channel.as_ref()?
+             .exchange_declare(exchange_opts).await.unwrap();
+
+         let (queue_name, _, _) = self.channel.as_ref()?
+             .queue_declare(QueueDeclareArguments::default())
+             .await
+             .unwrap()
+             .unwrap();
+
+         self.queue_name = queue_name;
+
+         self.channel.as_ref()?
+             .queue_bind(QueueBindArguments::new(
+                 &*self.queue_name.clone(),
+                 &*self.exchange_name.clone(),
+                 &*self.routing_key.clone()
+             ))
+             .await
+             .unwrap();
+
+        Some(())
+    }
+
+    pub async fn close(&mut self) -> Option<()> {
+        if !self.is_connection_good().await {
+            return None
+        }
+
+        mem::replace(&mut self.channel, None)?.close().await.unwrap();
+
+        Some(())
+    }
+
+    async fn is_connection_good(&self) -> bool {
+        if !self.open || self.flow_blocked || !self.connection.lock().await.is_connection_good() {
+            return false;
+        }
+        return true;
+    }
+
+    pub async fn publish_message(&self, message: String) -> Option<()> {
+        if !self.is_connection_good().await {
+            return None
+        }
+
+        let content = message.into_bytes();
+
+        let args = BasicPublishArguments::new(&*self.exchange_name.clone(), &*self.routing_key.clone());
+
+        self.channel.as_ref()?
+            .basic_publish(BasicProperties::default(), content, args)
+            .await
+            .unwrap();
+
+        Some(())
+    }
+
+    pub async fn attach_consumer<F>(&mut self, name: String, consumer: F) -> Option<()>
+    where
+        F: AsyncConsumer + Send + 'static
+    {
+        if !self.is_connection_good().await {
+            return None
+        }
+
+        let args = BasicConsumeArguments::new(
+            &*self.queue_name.clone(),
+            &*name
+        );
+
+        self.channel.as_ref()?
+            .basic_consume(consumer, args)
+            .await
+            .unwrap();
+
+        Some(())
+    }
+}
 
 pub struct Client {
     pub host: String,
     pub port: String,
     pub username: String,
     pub password: String,
-    pub exchange_name: String,
-    pub routing_key: String,
-    pub(crate) queue_name: String,
     pub(crate) connection: Option<Connection>,
-    pub(crate) channel: Option<Channel>,
+    pub(crate) channels: Vec<Arc<Mutex<Channel>>>,
     pub(crate) open: bool,
-    pub(crate) blocked: bool,
-    pub(crate) flow_blocked: bool
+    pub(crate) blocked: bool
 }
+
 static CLIENTS: Mutex<Vec<Arc<Mutex<Client>>>> = Mutex::const_new(Vec::new());
+static CHANNELS: Mutex<Vec<Arc<Mutex<Channel>>>> = Mutex::const_new(Vec::new());
 
 pub struct BasicConnectionCallback;
 
@@ -92,53 +212,53 @@ pub struct BasicChannelCallback;
 
 #[async_trait]
 impl ChannelCallback for BasicChannelCallback {
-    async fn close(&mut self, channel: &Channel, _close: CloseChannel) -> Result<()> {
-        let clients = CLIENTS.lock().await;
-        for client_mutex in clients.iter() {
-            let mut client = client_mutex.lock().await;
-            if client.open && client.connection.is_some() {
-                let client_channel = client.channel.as_ref().unwrap();
+    async fn close(&mut self, channel: &AMQPRS_Channel, _close: CloseChannel) -> Result<()> {
+        let channels = CHANNELS.lock().await;
+        for channel_mutex in channels.iter() {
+            let mut client_object = channel_mutex.lock().await;
+            if client_object.open && client_object.connection.lock().await.connection.is_some() {
+                let client_channel = client_object.channel.as_ref().unwrap();
                 if ptr::eq(client_channel, channel) {
-                    client.close_client().await;
+                    client_object.close().await;
                 }
             }
         }
         Ok(())
     }
-    async fn cancel(&mut self, channel: &Channel, _cancel: Cancel) -> Result<()> {
-        let clients = CLIENTS.lock().await;
-        for client_mutex in clients.iter() {
-            let mut client = client_mutex.lock().await;
-            if client.open && client.connection.is_some() {
-                let client_channel = client.channel.as_ref().unwrap();
+    async fn cancel(&mut self, channel: &AMQPRS_Channel, _cancel: Cancel) -> Result<()> {
+        let channels = CHANNELS.lock().await;
+        for channel_mutex in channels.iter() {
+            let mut chanel_object = channel_mutex.lock().await;
+            if chanel_object.open && chanel_object.connection.lock().await.connection.is_some() {
+                let client_channel = chanel_object.channel.as_ref().unwrap();
                 if ptr::eq(client_channel, channel) {
-                    client.close_client().await;
+                    chanel_object.close().await;
                 }
             }
         }
         Ok(())
     }
-    async fn flow(&mut self, channel: &Channel, active: bool) -> Result<bool> {
-        let clients = CLIENTS.lock().await;
-        for client_mutex in clients.iter() {
-            let mut client = client_mutex.lock().await;
-            if client.open && client.connection.is_some() {
-                let client_channel = client.channel.as_ref().unwrap();
+    async fn flow(&mut self, channel: &AMQPRS_Channel, active: bool) -> Result<bool> {
+        let channels = CHANNELS.lock().await;
+        for channel_mutex in channels.iter() {
+            let mut channel_object = channel_mutex.lock().await;
+            if channel_object.open && channel_object.connection.lock().await.connection.is_some() {
+                let client_channel = channel_object.channel.as_ref().unwrap();
                 if ptr::eq(client_channel, channel) {
-                    client.flow_blocked = active;
+                    channel_object.flow_blocked = active;
                 }
             }
         }
         Ok(active)
     }
-    async fn publish_ack(&mut self, channel: &Channel, ack: Ack) {
+    async fn publish_ack(&mut self, channel: &AMQPRS_Channel, ack: Ack) {
         println!(
             "Info: handle publish ack delivery_tag={} on channel {}",
             ack.delivery_tag(),
             channel
         );
     }
-    async fn publish_nack(&mut self, channel: &Channel, nack: Nack) {
+    async fn publish_nack(&mut self, channel: &AMQPRS_Channel, nack: Nack) {
         println!(
             "Warning: handle publish nack delivery_tag={} on channel {}",
             nack.delivery_tag(),
@@ -147,7 +267,7 @@ impl ChannelCallback for BasicChannelCallback {
     }
     async fn publish_return(
         &mut self,
-        channel: &Channel,
+        channel: &AMQPRS_Channel,
         ret: Return,
         _basic_properties: BasicProperties,
         content: Vec<u8>,
@@ -162,20 +282,16 @@ impl ChannelCallback for BasicChannelCallback {
 }
 
 impl Client {
-    pub async fn new(host: String, port: String, username: String, password: String, exchange_name: String, routing_key: String) -> Arc<Mutex<Client>> {
-        let client = Arc::new(Mutex::new(Client{
+    pub async fn new(host: String, port: String, username: String, password: String) -> Arc<Mutex<Client>> {
+        let client = Arc::new(Mutex::new(Client {
             host,
             port,
             username,
             password,
-            exchange_name,
-            routing_key,
-            queue_name: "".to_string(),
             connection: None,
-            channel: None,
+            channels: vec!(),
             open: false,
-            blocked: false,
-            flow_blocked: false,
+            blocked: false
         }));
 
         CLIENTS.lock().await.push(client.clone());
@@ -185,16 +301,15 @@ impl Client {
 
     pub fn is_connection_good(&self) -> bool
     {
-        if !self.open || self.blocked || self.flow_blocked {
+        if !self.open || self.blocked {
             return false;
         }
         return true;
     }
 
-    pub async fn open_client<F, G>(&mut self, connection_callback: F, channel_callback: G) -> Option<()>
-        where
-            F: ConnectionCallback + Send + 'static,
-            G: ChannelCallback + Send + 'static,
+    pub async fn open_client<F>(&mut self, connection_callback: F) -> Option<()>
+    where
+        F: ConnectionCallback + Send + 'static,
     {
         let mut attempt_cnt = 5;
         loop {
@@ -222,35 +337,6 @@ impl Client {
             .await
             .unwrap();
 
-        self.channel = Some(self.connection.as_ref()?.open_channel(None).await.unwrap());
-        self.channel.as_ref()?
-            .register_callback(channel_callback)
-            .await
-            .unwrap();
-
-        let mut exchange_opts = ExchangeDeclareArguments::new(&self.exchange_name.clone(), "topic");
-        exchange_opts.durable = true;
-
-        self.channel.as_ref()?
-            .exchange_declare(exchange_opts).await.unwrap();
-
-        let (queue_name, _, _) = self.channel.as_ref()?
-            .queue_declare(QueueDeclareArguments::default())
-            .await
-            .unwrap()
-            .unwrap();
-
-        self.queue_name = queue_name;
-
-        self.channel.as_ref()?
-            .queue_bind(QueueBindArguments::new(
-                &*self.queue_name.clone(),
-                &*self.exchange_name.clone(),
-                &*self.routing_key.clone()
-            ))
-            .await
-            .unwrap();
-
         self.open = true;
 
         Some(())
@@ -261,48 +347,27 @@ impl Client {
             return None
         }
 
-        mem::replace(&mut self.channel, None)?.close().await.unwrap();
+        for channel in &mut self.channels {
+            channel.lock().await.close().await;
+        }
+        self.channels.clear();
+
         mem::replace(&mut self.connection, None)?.close().await.unwrap();
 
         self.open = false;
         Some(())
     }
 
-    pub async fn publish_message(&self, message: String) -> Option<()> {
-        if !self.is_connection_good() {
-            return None
-        }
-
-        let content = message.into_bytes();
-
-        let args = BasicPublishArguments::new(&*self.exchange_name.clone(), &*self.routing_key.clone());
-
-        self.channel.as_ref()?
-            .basic_publish(BasicProperties::default(), content, args)
-            .await
-            .unwrap();
-
-        Some(())
-    }
-
-    pub async fn attach_consumer<F>(&mut self, name: String, consumer: F) -> Option<()>
-        where
-            F: AsyncConsumer + Send + 'static
+    pub async fn open_channel<F>(&mut self, connection: Arc<Mutex<Client>>, exchange_name: String, routing_key: String, channel_callback: F)
+        ->Option<Arc<Mutex<Channel>>>
+        where F: ChannelCallback + Send + Sized + 'static
     {
-        if !self.is_connection_good() {
-            return None
-        }
+        let rv = Channel::new(connection, exchange_name, routing_key).await;
 
-        let args = BasicConsumeArguments::new(
-            &*self.queue_name.clone(),
-            &*name
-        );
+        rv.lock().await.open_channel(channel_callback).await;
 
-        self.channel.as_ref()?
-            .basic_consume(consumer, args)
-            .await
-            .unwrap();
+        self.channels.push(rv.clone());
 
-        Some(())
+        Some(rv)
     }
 }
